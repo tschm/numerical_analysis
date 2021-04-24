37a83ce9b34d56b9533676c3cc3dfb446ee846ce
Thibault Bourgeron <thibault.bourgeron@protonmail.com>
2021-04-24 04:42:43 -0400

    add tests + documentation

--- nearest_corr_mat/README.md
+++ nearest_corr_mat/README.md
@@ -1,19 +1,39 @@
-This package is intended for a fast computation of the nearest correlation matrix to a given positive semidefinite matrix, in the Euclidian sense.
+This package is intended for a fast computation of the nearest correlation matrix, in the Euclidian sense.
 
-Four methods are implemented in the main function nearest_corr:
-- 'dual_grad' is based on [Ingham](https://www.maths.manchester.ac.uk/~higham/narep/narep369.pdf) and consists in a modified Dykstra's method. As noted in [Malick](https://hal.inria.fr/inria-00072409v2/document) (and this can be checked in the code) it is exactly a gradient descent for the dual problem.
-- 'dual_bfgs' is a BFGS method for the dual problem, as proposed in Malick's. The implementations are based on the [textbook](https://www.springer.com/gp/book/9783540354451).
-- 'dual_l_bfgs' is a memory limited BFGS method for the dual problem, as suggested in Malick's. The implementations are based on the same book.
-- 'dual_newton' is the Newton's method for the dual problem studied in [Qi, Sun](http://www.personal.soton.ac.uk/hdqi/REPORTS/simax_06.pdf).
+# Problem and its dual
 
-On random 100 x 100 matrices, on a given machine, the convergences take, for:
-- 'dual_grad': 25 s and 2000 iterations,
-- 'dual_bfgs' 2 s and 150 iterations,
-- 'dual_l_bfgs': 2 s and 150 iterations,
-- 'dual_newton': 100 s and 150 iterations.
+A correlation matrix is a real symmetric positive semidefinite matrix with unit diagonal.
+The problem is to find a the closest correlation matrix to a given positive semidefinite matrix, for the Frobenius norm.
+See *README.pdf* for some extra details.
 
-The (complicated, double-loop) construction of the Newton's matrix for 'dual_newton' may be responsible for this algorithm to be slowest, for this set of parameters. For higher dimension problems (1000 x 1000), the method 'dual_l_bfgs' seems to be the fastest (when memory=10 remains unchanged).
+The more use of the regularity of these functionals is performed, the more efficient the algorithm, theoretically.
 
-For none of the method a line-search is performed because a Wolfe's (or a even simpler) line search considerably slows down all the methods and setting the step to 1 is a simple and good approximation (this is consistent with a remark in Qi, Sun and with the initial value for quasi-Newton methods; Wolfe's criterium is often satisfied for 1). For 'dual-newton', for the same speed reason, the conjugate gradient is not used; Numpy's solve is used instead, and the direction checking is not performed.
+Although the algorithms below were not published recently, I am not aware of any freely accessible implementation, apart from the Python module Statsmodels which implements the simplest algorithm.
 
-For all the methods, for err_max = 1e-6, the convergence seem to be linear. The theoretical superlinear convergences of 'dual_bfgs', 'dual_l_bfgs', when the Wolfe's line search is sought, and quadratic convergence of 'dual_newton' have not been observed but may be for other range of parameters (smaller values of err_max for instance).
+# Four algorithms implemented
+
+As the problem is constrained in the intersection of two convex sets, Von Neumann alternative projection can be used, *cf. performances.ipynb*. This algorithm may converge to a sub optimal solution (as the cone of psd matrices is not a vector space). Dykstra's projection does not converge at all, *cf. performances.ipynb.*
+
+Four methods, are, either indirectly or directly, based on the dual problem, and are implemented in the main function *nearest_corr*.
+
+1. The algorithm 'grad' is based on [Higham, 2002](https://www.maths.manchester.ac.uk/~higham/narep/narep369.pdf). It is a modified Dykstra's projection algorithm, adapted to find a point in the intersection of an affine subspace and convex subset. As noted in [Malick, 2004](https://hal.inria.fr/inria-00072409v2/document), in a general way, this algorithm is exactly a gradient descent for the dual problem.
+2. The algorithm 'bfgs' is the BFGS algorithm applied to the convex, differentiable, unconstrained dual problem, as suggested in Malick. The implementations are based on [BGLS textbook](https://link.springer.com/book/10.1007/978-3-540-35447-5).
+3. The algorithm 'l_bfgs' is the limited BFGS algorithm applied to the dual problem, as suggested in Malick. The implementations are based on the same book.
+4. The algorithm 'newton' is a Newton's method for the dual problem. It uses the strong semismoothness of the gradient of the dual functional and an explicit construction of elements of its Clarke Jacobian. It has been developed in [Qi, Sun, 2006](http://www.personal.soton.ac.uk/hdqi/REPORTS/simax_06.pdf).
+
+# Performances
+
+On random 100 x 100 matrices (*cf. test.py*), for err_max=1e-6,  on a given machine, the algorithms have the following performances.
+
+| algo   | time  | n_iterations |
+| ------ | ----- | ------------ |
+| grad   | 20 s  | 1500         |
+| bfgs   | 2 s   | 100          |
+| l-bfgs | 2 s   | 100          |
+| newton | 60 s  | 100          |
+
+The (complicated, double-loop) construction of the Newton's matrix for 'newton' algorithm may be responsible for this algorithm to be slowest, for this set of parameters. For higher dimension problems (1000 x 1000), the method 'l_bfgs' seems to be the fastest (when 'memory' remains unchanged).
+
+For none of the method a line search is performed because a Wolfe (or a even simpler) line search considerably slows down all the methods and setting the step to 1 is a simple and good enough (this is consistent with a remark in Qi, Sun and with the initial value for quasi-Newton methods; experimentaly, Wolfe condition is often satisfied for value 1). For 'newton', for the same speed reason, the conjugate gradient is not used; Numpy's solve is used instead, and the direction checking is not performed.
+
+For all the methods, for err_max=1e-6, the convergence seem to be linear. This is expected for the modified Dysktra's projection. Quasi-Newton methods converge superlinearly when the functional is smooth and a Wolfe line search is performed, for instance. The algorithms 'bfgs', 'l_bfgs' seem to converge only linearly, maybe because the gradient of the functional is only semismooth. It is surprising for the Newton's method to have only a linear convergence, because it is proved to be quadratically convergent. The quadratic convergence may be observed for other ranges of parameters, as noted in Qi, Sun.
--- nearest_corr_mat/src/nearest_corr_mat_pkg/functions.py
+++ nearest_corr_mat/src/nearest_corr_mat_pkg/functions.py
@@ -2,65 +2,180 @@ from itertools import product
 from functools import reduce
 import numpy as np
 from numpy.linalg import norm, eig, solve
+from scipy.sparse import diags
 
 
-def nearest_corr(mat, method='dual_bfgs', err_max=None, ite_max=None, **kwargs):
-    default_params = {
-        'dual_grad': (1e-6, 10000),
-        'dual_bfgs': (1e-6, 1000),
-        'dual_l_bfgs': (1e-6, 1000),
-        'dual_newton': (1e-6, 1000),
-    }
-    if err_max is None:
-        err_max = default_params[method][0]
-    if ite_max is None:
-        ite_max = default_params[method][1]
-
-    if method == 'dual_grad':
-        res = proj_ingham(mat, err_max, ite_max)
-    elif method == 'dual_bfgs':
-        res = bfgs(mat, err_max, ite_max)
-    elif method == 'dual_l_bfgs':
-        res = l_bfgs(mat, err_max, ite_max, **kwargs)  # memory
-    elif method == 'dual_newton':
-        res = proj_qi_sun(mat, err_max, ite_max)
-    return res
+DIM = 10
+diag_1 = [-1] * (DIM - 1)
+diagonals = [diag_1, [2] * DIM, diag_1]
+laplace = diags(diagonals, [-1, 0, 1]).toarray()
 
 
-rng = np.random.default_rng()
-def gen_sym(int_n, int_p=None):
+def gen_sym_psd(int_n, seed=None, int_p=None):
+    '''Generates a random symmetric positive semidefinite array.
+
+    Parameters
+    ----------
+    int_n : int
+        Size of the output array.
+    seed : {None, int}, optional
+        Random seed used by `numpy.random.default_rng`.
+    int_p : {None, int}, optional
+        Almost surely the rank of the output matrix is `min(int_p, int_n)`.
+
+    Returns
+    -------
+    mat : array
+        Random symmetric positive semidefinite array.
+
+    '''
     if int_p is None:
         int_p = int_n
-    mat = rng.standard_normal((int_n, int_p))
-#     mat = mat / norm(mat, axis=0)
-    return mat.T @ mat
+    rng = np.random.default_rng(seed)
+    draws = rng.standard_normal((int_n, int_p))
+    return draws @ draws.T
+
 
 def proj_unit_diag(mat):
+    '''Return a copy of an array, which diagonal is filled with ones.
+
+    Parameters
+    ----------
+    mat : array
+        Input array.
+
+    Returns
+    -------
+    mat_ : array
+        Output array with unit diagonal.
+
+    '''
     mat_ = mat.copy()
     np.fill_diagonal(mat_, 1)
     return mat_
 
+
 def conj(mat_1, mat_2):
-    '''Computes mat_1 @ mat_2 @ mat_1.T. If mat_2 is an array or a list,
-    the previous formula is applied to mat_2 = diag(mat_2)'''
+    '''Compute the conjugation of `mat_2` by `mat_1`.
+
+    Computes `mat_1 @ mat_2 @ mat_1.T`. If `mat_2` is a flat array or a list,
+    the previous formula is applied to `mat_2 = diag(mat_2)`.
+
+    Parameters
+    ----------
+    mat_1 : array
+        Input square array.
+    mat_2 : {array, list}
+        Input square array or flat array or vector.
+
+    Returns
+    -------
+    mat : array
+        The result of the conjugation of `mat_2` by `mat_1`.
+
+    '''
     mat = np.diag(mat_2) if np.array(mat_2).ndim == 1 else mat_2
     return reduce(np.matmul, [mat_1, mat, mat_1.T])
 
+
 def real_eig(mat):
+    '''Compute the eigenelements of a real symmetric array.
+
+    Parameters
+    ----------
+    mat : array
+        Input real symmetric array.
+
+    Returns
+    -------
+    w : array
+        The eigenvalues.
+    v : array
+        The normalized eigenvectors.
+
+    '''
     diag, t_mat = eig(mat)
     return np.real(diag), np.real(t_mat)
 
+
 def proj_cone(mat):
+    '''Project onto the cone of real symmetric positive semidefinite arrays.
+
+    Parameters
+    ----------
+    mat : array
+        Input real symmetric array.
+
+    Returns
+    -------
+    mat_ : array
+        The projection of the input array.
+
+    '''
     diag, t_mat = real_eig(mat)
     return conj(t_mat, np.maximum(diag, 0))
 
-def argmin(var_dual, mat_g):
-    return mat_g + np.diag(var_dual)
+
+def dual_to_primal(var_dual, mat):
+    '''Build a primal variable from a dual variable.
+
+    Parameters
+    ----------
+    var_dual : array
+        Input flat array.
+    mat : array
+        Input square array.
+
+    Returns
+    -------
+    var_prim : array
+        Output square array.
+
+    '''
+    return mat + np.diag(var_dual)
+
 
 def grad_theta(arg_dual):
+    '''Evaluate the gradient of the functional.
+
+    Parameters
+    ----------
+    arg_dual : array
+        Input real symmetric array.
+
+    Returns
+    -------
+    mat_ : array
+        Output square array.
+
+    '''
     return np.diag(proj_cone(arg_dual)) - np.ones(len(arg_dual))
 
-def proj_ingham(mat, err_max, ite_max):
+
+def proj_grad(mat, err_max, ite_max):
+    '''Compute the nearest correlation matrix.
+
+    Uses a modified Dijkstra's algorithm, cf. Algorithm 3.3 in:
+    Higham, 2002 https://www.maths.manchester.ac.uk/~higham/narep/narep369.pdf
+
+    Parameters
+    ----------
+    mat : array
+        Input real positive semidefinite array to be projected.
+    err_max : float
+        Maximum absolute error of the algorithm.
+    ite_max : int
+        Maximum number of iterations performed by the algorithm.
+
+    Returns
+    -------
+    mat_ : array
+        Nearest correlation matrix (real symmetric positive semidefinite
+        array with unit diagonal).
+    errors : list
+        List of the successive update errors.
+
+    '''
     err_prim = err_max + 1
     ite = 0
     prim_2 = mat.copy()
@@ -80,8 +195,28 @@ def proj_ingham(mat, err_max, ite_max):
         ite += 1
     return 0.5 * (prim_1 + prim_2), errs
 
+
 def update_bfgs(inv_hess, dvar, dgrad):
-    "Section 4.4, equation (BFGS), p. 55"
+    '''Rank 2 update of the inverse of the Hessian matrix used in BFGS algorithm.
+
+    See BGLS textbook Section 4.4, equation (BFGS), p. 55.
+
+    Parameters
+    ----------
+    inv_hess : array
+        Input square array, previous approximation of the inverse of the Hessian.
+    dvar : array
+        Flat array, difference between current and previous points.
+    dgrad : array
+        Flat array, difference between current and previous gradients.
+
+    Returns
+    -------
+    mat_ : array
+        Output square array, update to compute the new approximation
+        of the inverse of the Hessian matrix.
+
+    '''
     dot = np.dot(dvar, dgrad)
     mat_1 = np.outer(dvar, dgrad) @ inv_hess
     mat_1 = mat_1 + mat_1.T
@@ -89,10 +224,35 @@ def update_bfgs(inv_hess, dvar, dgrad):
     mat_2 = (1 + mat_2 / dot) * np.outer(dvar, dvar)
     return (-mat_1 + mat_2) / dot
 
-def bfgs(mat_g, err_max, ite_max):
-    dim = len(mat_g)
-    var_dual = np.zeros(dim)
-    grad_theta_old = grad_theta(argmin(var_dual, mat_g))
+
+def proj_bfgs(mat, err_max, ite_max):
+    '''Compute the nearest correlation matrix.
+
+    Uses BFGS algorithm for the dual problem, cf. Algorithm 1 in:
+    Malick, 2004 https://hal.inria.fr/inria-00072409v2/document
+    The implementation is based on BGLS textbook, chapter 4.
+
+    Parameters
+    ----------
+    mat : array
+        Input real positive semidefinite array to be projected.
+    err_max : float
+        Maximum absolute error of the algorithm.
+    ite_max : int
+        Maximum number of iterations performed by the algorithm.
+
+    Returns
+    -------
+    mat_ : array
+        Nearest correlation matrix (real symmetric positive semidefinite
+        array with unit diagonal).
+    errors : list
+        List of the successive update errors.
+
+    '''
+    dim = len(mat)
+    var_dual = np.ones(dim) - np.diag(mat)
+    grad_theta_old = grad_theta(dual_to_primal(var_dual, mat))
     inv_hess = np.eye(dim)
     err = err_max + 1
     ite = 0
@@ -103,18 +263,39 @@ def bfgs(mat_g, err_max, ite_max):
         var_dual = var_dual - direc
 
         ## step 2: update and error computation
-        grad_theta_new = grad_theta(argmin(var_dual, mat_g))
+        grad_theta_new = grad_theta(dual_to_primal(var_dual, mat))
         diff_grads = grad_theta_new - grad_theta_old
-        inv_hess = inv_hess + update_bfgs(
-            inv_hess, -direc, diff_grads)
+        inv_hess = inv_hess + update_bfgs(inv_hess, -direc, diff_grads)
         err = norm(grad_theta_old)
         grad_theta_old = grad_theta_new.copy()
         errs.append(err)
         ite += 1
-    return proj_cone(argmin(var_dual, mat_g)), errs
+    return proj_cone(dual_to_primal(var_dual, mat)), errs
+
 
 def inv_hess_limited(grad_theta_val, dvars, dgrads):
-    "Algorithms 6.6, 6.8, p. 85"
+    '''Compute the approximation of the inverse of the Hessian matrix used in L-BFGS.
+
+    The computation is based on differences of previous points / gradients,
+    cf. BGLS textbook, Algorithms 6.6, 6.8, p. 85.
+
+    Parameters
+    ----------
+    grad_theta_val : array
+        Input flat array, current value of the gradient.
+    dvars : list
+        List of flat arrays, which are the differences of the previously computed points.
+        The size of this list is the memory of the algorithm.
+    dgrads : list
+        List of flat arrays, which are the differences of the previously computed gradients.
+        The size of this list is the memory of the algorithm.
+
+    Returns
+    -------
+    mat_ : array
+        Output square array, new approximation of the inverse of the Hessian matrix.
+
+    '''
     alphas = []
     q_i = grad_theta_val.copy()
     for dvar, dgrad in zip(dvars[::-1], dgrads[::-1]):
@@ -127,10 +308,37 @@ def inv_hess_limited(grad_theta_val, dvars, dgrads):
         h_i = h_i + (alpha - beta) * dvar
     return h_i
 
-def l_bfgs(mat_g, err_max, ite_max, memory=10):
-    dim = len(mat_g)
-    var_dual = np.zeros(dim)
-    grad_theta_old = grad_theta(argmin(var_dual, mat_g))
+
+def proj_l_bfgs(mat, err_max, ite_max, memory=10):
+    '''Compute the nearest correlation matrix.
+
+    Uses L-BFGS algorithm for the dual problem, cf. Algorithm 1 in:
+    Malick, 2004 https://hal.inria.fr/inria-00072409v2/document
+    The implementation is based on BGLS textbook, chapter 6.
+
+    Parameters
+    ----------
+    mat : array
+        Input real positive semidefinite array to be projected.
+    err_max : float
+        Maximum absolute error of the algorithm.
+    ite_max : float
+        Maximum number of iterations performed by the algorithm.
+    memory : int, optional
+        Limited memory of the algorithm.
+
+    Returns
+    -------
+    mat_ : array
+        Nearest correlation matrix (real symmetric positive semidefinite
+        array with unit diagonal).
+    errors : list
+        List of the successive update errors.
+
+    '''
+    dim = len(mat)
+    var_dual = np.ones(dim) - np.diag(mat)
+    grad_theta_old = grad_theta(dual_to_primal(var_dual, mat))
     inv_hess = np.eye(dim)
     dvars = []
     dgrads = []
@@ -146,27 +354,41 @@ def l_bfgs(mat_g, err_max, ite_max, memory=10):
         var_dual = var_dual - direc
 
         ## step 2: update and error computation
-        grad_theta_new = grad_theta(argmin(var_dual, mat_g))
+        grad_theta_new = grad_theta(dual_to_primal(var_dual, mat))
         diff_grads = grad_theta_new - grad_theta_old
 
         if ite < memory:
-            inv_hess = inv_hess + update_bfgs(
-                inv_hess, -direc, diff_grads)
-            dvars.append(-direc)
-            dgrads.append(diff_grads)
+            inv_hess = inv_hess + update_bfgs(inv_hess, -direc, diff_grads)
         else:
             del dvars[0]
             del dgrads[0]
-            dvars.append(-direc)
-            dgrads.append(diff_grads)
+        dvars.append(-direc)
+        dgrads.append(diff_grads)
 
         err = norm(grad_theta_old)
         grad_theta_old = grad_theta_new.copy()
         errs.append(err)
         ite += 1
-    return proj_cone(argmin(var_dual, mat_g)), errs
+    return proj_cone(dual_to_primal(var_dual, mat)), errs
+
 
 def build_newton_mat(arg_dual):
+    '''Compute an element of the Clarke Jacobian of the gradient of the functional.
+
+    Cf. 5. a) "Forming the Newton matrix" in:
+    Qi, Sun, 2006 http://www.personal.soton.ac.uk/hdqi/REPORTS/simax_06.pdf
+
+    Parameters
+    ----------
+    arg_dual : array
+        Input real symmetric array.
+
+    Returns
+    -------
+    mat_ : array
+        An element of the Clarke Jacobian of the gradient of the functional at `arg_dual`.
+
+    '''
     dim = len(arg_dual)
     diag, t_mat = real_eig(arg_dual)
 
@@ -185,40 +407,92 @@ def build_newton_mat(arg_dual):
             diag[ind_1] / (diag[ind_1] - diag[ind_2]))
         newton_mat[ind_2, ind_1] = (
             diag[ind_2] / (diag[ind_2] - diag[ind_1]))
-
     v_y = [
         np.diag(conj(t_mat, newton_mat * conj(
             t_mat.T, [0] * ind + [1] + [0] * (dim - ind -1))))
         for ind in range(dim)]
     return np.array(v_y)
 
-def check_direction(newton_mat, grad, direc, eta, tol):
-    norm_grad = norm(grad)
-    eta_k = min(eta, norm_grad)
-    norm_lhs = norm(grad - newton_mat @ direc)
-    norm_rhs = eta_k * norm_grad
-    ascent = np.dot(grad, direc) + eta_k * norm(direc)
-    if norm_lhs >= norm_rhs + tol or -ascent <= tol:
-        direc = grad
-    return direc
 
-def proj_qi_sun(mat_g, err_max, ite_max):
-    var_dual = np.ones(len(mat_g)) - np.diag(mat_g)
+def proj_newton(mat, err_max, ite_max):
+    '''Compute the nearest correlation matrix.
+
+    Uses a Newton method for the dual problem, cf. Algorithm 5.1 in:
+    Qi, Sun, 2006 http://www.personal.soton.ac.uk/hdqi/REPORTS/simax_06.pdf
+
+    Parameters
+    ----------
+    mat : array
+        Input real positive semidefinite array to be projected.
+    err_max : float
+        Maximum absolute error of the algorithm.
+    ite_max : int
+        Maximum number of iterations performed by the algorithm.
+
+    Returns
+    -------
+    mat_ : array
+        Nearest correlation matrix (real symmetric positive semidefinite
+        array with unit diagonal).
+    errors : list
+        List of the successive update errors.
+
+    '''
+    var_dual = np.ones(len(mat)) - np.diag(mat)
 
     err = err_max + 1
     ite = 0
     errs = list()
     while err > err_max and ite < ite_max:
         ## step 1: dual descent direction direc
-        arg_dual = argmin(var_dual, mat_g)
+        arg_dual = dual_to_primal(var_dual, mat)
         newton_mat = build_newton_mat(arg_dual)
         grad = grad_theta(arg_dual)
         direc = solve(newton_mat, grad)
-#         direc = check_direction(newton_mat, grad, direc, 1e-5, 1e-10)
 
         ## step 2: update and error computation
         var_dual = var_dual - direc
         err = norm(direc)
         errs.append(err)
         ite += 1
-    return proj_cone(argmin(var_dual, mat_g)), errs
+    return proj_cone(dual_to_primal(var_dual, mat)), errs
+
+
+def nearest_corr(mat, algo='bfgs', err_max=None, ite_max=None, **kwargs):
+    '''Compute the nearest correlation matrix.
+
+    Parameters
+    ----------
+    mat : array
+        Input real positive semidefinite array to be projected.
+    algo : {'grad', 'bfgs', 'l_bfgs', 'newton'}, optional
+        Algorithm to be used. Default is `'bfgs'`.
+    err_max : {None, float}, optional
+        Maximum absolute error of the algorithm.
+    ite_max : {None, int}, optional
+        Maximum number of iterations performed by the algorithm.
+    kwargs : dict, optional
+        The parameter `memory` can be passed to the algo `'l_bfgs'` this way.
+
+    Returns
+    -------
+    mat_ : array
+        Nearest correlation matrix (real symmetric positive semidefinite
+        array with unit diagonal).
+    errors : list
+        List of the successive update errors.
+
+    '''
+    mat = 0.5 * (mat + np.array(mat).T)  # cf. Qi Sun, 4.3
+    algos_params = {
+        'grad': (proj_grad, 1e-6, 10000),
+        'bfgs': (proj_bfgs, 1e-6, 1000),
+        'l_bfgs': (proj_l_bfgs, 1e-6, 1000),
+        'newton': (proj_newton, 1e-6, 1000),
+    }
+    if err_max is None:
+        err_max = algos_params[algo][1]
+    if ite_max is None:
+        ite_max = algos_params[algo][2]
+
+    return algos_params[algo][0](mat, err_max, ite_max, **kwargs)
