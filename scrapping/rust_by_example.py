cf7292e8f0276fdde2d6d0ef5f8669bffe9733b4
Thibault Bourgeron <thibault.bourgeron@protonmail.com>
2021-09-25 00:50:23 -0400

    walrus

--- scrapping/rust_by_example.py
+++ scrapping/rust_by_example.py
@@ -1,5 +1,6 @@
 from os import mkdir, chdir
 from subprocess import Popen
+from collections import defaultdict
 from requests import get
 from bs4 import BeautifulSoup
 
@@ -7,22 +8,17 @@ BASE_URL = 'https://doc.rust-lang.org/rust-by-example/'
 text = BeautifulSoup(get(BASE_URL + 'index.html').text, features='html.parser')
 links = [link.get('href') for link in text.find_all('a', href=True)][1:192]
 
-# split the links into different groups
-old = 'hello'
+# put the links in different groups
+old = None
 count = 0
-name = str(count).zfill(2) + '_' + old
-groups = {name: []}
+groups = defaultdict(list)
 for link in links:
-    beginning = link[:-5].split('/')[0]
-    if beginning == old:
-        groups[name].append(link)
-    else:
+    if (beginning := link[:-5].split('/')[0]) != old:
         old = beginning
         count += 1
-        name = str(count).zfill(2) + '_' + old
-        groups[name] = [link]
+    groups[str(count).zfill(2) + '_' + old].append(link)
 
-# load the Rust snippets in each html page and compile them
+# load the rust snippets in each html page and compile them
 for group, links in groups.items():
     mkdir(group)
     chdir(group)
@@ -36,7 +32,6 @@ for group, links in groups.items():
             file.close()
             proc = Popen(['rustc', full_name])
             proc.communicate()
-            retcode = proc.returncode
-            if retcode != 0:
+            if (retcode := proc.returncode) != 0:
                 print(f'{full_name}: error {retcode} while compiling.')
     chdir('../')
